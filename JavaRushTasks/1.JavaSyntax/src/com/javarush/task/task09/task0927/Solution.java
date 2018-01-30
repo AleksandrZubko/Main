05e26bacd57fd25ff6d5b992d4ecfa26d5f23bfb
AleksandrZubko <zubko.alexander@gmail.com>
2018-01-30 17:58:40 +0200

    com.javarush.task.task09.task0927

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task09/task0927/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task09/task0927/Solution.java
@@ -1,4 +1,4 @@
-package com.javarush.task.task09.task0927;
+        package com.javarush.task.task09.task0927;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -18,12 +18,22 @@ public class Solution {
 
     public static Map<String, Cat> createMap() {
         //напишите тут ваш код
-
+        String[] cats = new String[] {"васька", "мурка", "дымка", "рыжик", "серый", "снежок", "босс", "борис", "визя", "гарфи"};
+        Map<String, Cat> addCats = new HashMap<String, Cat>();
+        for (String s : cats)
+        {
+            addCats.put(s, new Cat(s));
+        }
+        return addCats;
     }
 
     public static Set<Cat> convertMapToSet(Map<String, Cat> map) {
         //напишите тут ваш код
-
+        HashSet<Cat> set = new HashSet<Cat>();
+        for (Cat c:map.values()) {
+            set.add(c);
+        }
+        return set;
     }
 
     public static void printCatSet(Set<Cat> set) {
