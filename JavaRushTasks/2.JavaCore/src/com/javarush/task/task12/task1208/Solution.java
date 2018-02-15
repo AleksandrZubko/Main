b00586ca3ba908af0cce67133827d659d43069b1
AleksandrZubko <zubko.alexander@gmail.com>
2018-02-15 17:17:22 +0200

    7

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(7);
+        print("");
+        print(7.1);
+        print((Integer)2);
+        print('q');
+    }
+
+    //Напишите тут ваши методы
+    static void print(int i){
+
+    }
+    static void print(String i){
+
+    }
+    static void print(double i){
+
+    }
+    static void print(Integer i){
+
+    }
+    static void print(char i){
+
+    }
+}
---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static int max(int x, int y){
+        int max = x > y ? x : y;
+        return max;
+    }
+    public static long max(long x, long y){
+        long max = x > y ? x : y;
+        return max;
+    }
+    public static double max(double x, double y){
+        double max = x > y ? x : y;
+        return max;
+    }
+}
