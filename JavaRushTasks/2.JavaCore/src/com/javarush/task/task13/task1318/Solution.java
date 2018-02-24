45100f383b63f91f4fa31bff72a7927082d6e104
aleksandrzubko <zubko.alexander@gmail.com>
2018-02-24 23:29:20 +0200

    13

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task13.task1318;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.*;
+
+/* 
+Чтение файла
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception{
+        // напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sourceFileName = reader.readLine();      //считываем с ввода путь к файлу для чтения
+        InputStream inStream = new FileInputStream(sourceFileName);
+        while (inStream.available() > 0) { //available - определяет кол-во байт в файле (из класса FileInputStream)
+            char body = (char)inStream.read(); // считываем содержимое файла
+            System.out.print(body); //выводим содержимое файла на экран
+        }
+        inStream.close(); //закрываем потоки FileInputStream
+        reader.close(); //закрываем потоки BufferedReader
+    }
+}
