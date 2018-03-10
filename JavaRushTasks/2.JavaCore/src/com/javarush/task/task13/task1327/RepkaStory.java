35f7e1ef4c0cb1ce4babed323258c6878b90fcec
aleksandrzubko <zubko.alexander@gmail.com>
2018-03-10 18:13:14 +0200

    20

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1327/RepkaStory.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1327/RepkaStory.java
@@ -6,9 +6,9 @@ public class RepkaStory {
     static void tell(List<Person> items) {
         Person first;
         Person second;
-        for (int i = items.size() - 1; i > 0; i--) {
-            first = items.get(i - 1);
-            second = items.get(i);
+        for (int i = items.size()-1; i >0 ; i--) {
+            first = items.get(i);
+            second = items.get(i-1);
             first.pull(second);
         }
     }
