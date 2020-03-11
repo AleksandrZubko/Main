14898d81a69e7a424756c0e3e7bc4c21af42869e
DN230483ZAV2 <aleksandr.zubko@privatbank.ua>
2020-03-11 09:08:28 +0200

    getFileTree

--- Test/src/getFileTree/Main.java
+++ Test/src/getFileTree/Main.java
@@ -12,19 +12,27 @@ public class Main {
         for (String st: list) {
             System.out.println(st);
         }
-
     }
 }
 class Solution {
+    /**
+     * Поиск файлов в указанной директории без рекурсии
+     * @param root Директроия, в которой искать файлы
+     * @return List<String> с абсолютными путями к файлам
+     * @throws IOException Queue.remove() и File вызывает Exception
+     */
     public List<String> getFileTree(String root) throws IOException {
         File rootDir = new File(root);
         List<String> result = new ArrayList<>();
-        Queue<File> fileTree = new PriorityQueue<>();
 
-        Collections.addAll(fileTree, rootDir.listFiles());
+        //Интерфейс Queue расширяет Collection
+        Queue<File> fileTree = new PriorityQueue<>();  //PriorityQueue – это класс очереди с приоритетами, Элементу с наименьшим значением присваивается наибольший приоритет
+
+        //получить коллекцию на базе массива
+        Collections.addAll(fileTree, rootDir.listFiles());  //Метод Collections.addAll принимает на входе объект Collection и массив. Вместо массива также можно указать элементы через запятую
 
         while (!fileTree.isEmpty()) {
-            File currentFile = fileTree.remove();
+            File currentFile = fileTree.remove();  //удаляет элемент из головы очереди, возвращая его. Инициирует исключение NoSuchElementException, если очередь пуста
             if (currentFile.isDirectory()) {
                 Collections.addAll(fileTree, currentFile.listFiles());
             } else {
